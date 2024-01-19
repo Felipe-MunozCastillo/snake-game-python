a527fd3b000ac2baae39f2523118c3f710d094de
Felipe Muñoz <fmunoz.acastillo@gmail.com>
2024-01-19 15:32:30 -0300

    clase Snake revisada

--- snake.py
+++ snake.py
@@ -13,6 +13,7 @@ class Snake:
         self.head = self.my_snake[0]
 
     def create_snake(self, position_x, position_y):
+        """Metodo para crear la culebra."""
         for number in range(3):
             snake = Turtle(shape="square")
             snake.penup()
@@ -21,6 +22,7 @@ class Snake:
             self.my_snake.append(snake)
 
     def add_segment(self, position):
+        """Metodo para agregar un segmento a la serpiente."""
         snake = Turtle(shape="square")
         snake.penup()
         snake.color("white")
@@ -28,10 +30,10 @@ class Snake:
         self.my_snake.append(snake)
 
     def extend(self):
-        # add a new segment to the snake
         self.add_segment(self.my_snake[-1].position())
 
     def move(self):
+        """Método de movimiento de la serpiente."""
         for seg_num in range(len(self.my_snake) - 1, 0, -1):
             new_x = self.my_snake[seg_num - 1].xcor()
             new_y = self.my_snake[seg_num - 1].ycor()
@@ -39,24 +41,30 @@ class Snake:
         self.my_snake[0].forward(20)
 
     def up(self):
+        """Metodo para que la culebra se mueva hacia arriba."""
         if self.head.heading() != DOWN:
             self.head.seth(UP)
 
     def down(self):
+        """Metodo para que la culebra se mueva hacia abajo."""
         if self.head.heading() != UP:
             self.head.seth(DOWN)
 
     def left(self):
+        """Metodo para que la culebra se mueva hacia izquierda."""
         if self.head.heading() != RIGHT:
             self.head.seth(LEFT)
 
     def right(self):
+        """Metodo para que la culebra se mueva hacia derecha."""
         if self.head.heading() != LEFT:
             self.head.seth(RIGHT)
 
     def reset(self):
+        """Metodo de reinicio de la posicion de la culebra
+        al centro de la pantalla."""
         for seg in self.my_snake:
             seg.goto(1000, 1000)
         self.my_snake.clear()
-        self.create_snake(0,0)
+        self.create_snake(0, 0)
         self.head = self.my_snake[0]
