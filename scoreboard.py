892bba7485e65ad8a5bfaa643c1f4e58bad46ca2
Felipe Muñoz <fmunoz.acastillo@gmail.com>
2024-01-19 15:24:45 -0300

    clase ScoreBoard revisada

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 
 class ScoreBoard(Turtle):
+    """Clase para manejar el puntaje."""
 
     def __init__(self):
         super().__init__()
@@ -13,13 +14,18 @@ class ScoreBoard(Turtle):
         self.goto(0, 260)
 
     def add_score(self):
+        """Metodo para aumentar en 1 el puntaje."""
         self.clear()
         self.score += 1
 
     def show_score(self):
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 24, "normal"))
+        """Metodo para mostrar el puntaje actual y maximo."""
+        self.write(f"Score: {self.score} High Score: {self.high_score}",
+                   align="center", font=("Courier", 24, "normal"))
 
     def game_over(self):
+        """Metodo para mostrar el mensaje de GAME OVER cuando el 
+        jugador pierde."""
         self.goto(0, 0)
         self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
 
@@ -31,9 +37,12 @@ class ScoreBoard(Turtle):
         self.show_score()
 
     def find_high_score(self):
+        """Metodo para abrir y leer el archivo data.txt, 
+        obteniendo el puntaje maximo."""
         with open("data.txt", mode="r") as file:
             return int(file.read())
 
     def save_high_score(self):
+        """Metodo para guardar el puntaje máximo."""
         with open("data.txt", mode="w") as file:
             file.write(str(self.high_score))
